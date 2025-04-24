736660ca4d58cb2ac765f9a5f5e3ee1281412144
Johan Henriksson <mahogny@areta.org>
2025-04-24 12:37:36 +0200

    fix

--- prep_rna.R
+++ prep_rna.R
@@ -120,6 +120,7 @@ system("echo END totirp >> time.txt; echo `date +%s` >> time.txt")
 system("echo START BascetAlignToReference >> time.txt; echo `date +%s` >> time.txt")
 BascetAlignToReference(
   bascetRoot,
+  inputName = "new_filtered",
   useReference="/home/m/mahogny/mystore/atrandi/bwa_ref/yersinia/all.fa",
   numLocalThreads=10
 )
